8ee224abbae697f23884c607fa94e9a5fc38390c
Tram Anh <tramanh0105@gmail.com>
2019-06-03 20:00:41 +0200

    added UserService

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url = `http://localhost:8081`;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(): Observable<User[]> {
+    const newUrl = `${this.url}/users`;
+    return this.http.get<User[]>(newUrl);
+  }
+
+  updateUser(user: User, userId: number): Observable<User> {
+    const newUrl = `${this.url}/users/${userId}`;
+    return this.http.put<User>(newUrl, null);
+  }
+
+  deleteUser(userId: number): Observable<User> {
+    const newUrl = `${this.url}//users/${userId}`;
+    return this.http.delete<User>(newUrl);
+  }
+}
