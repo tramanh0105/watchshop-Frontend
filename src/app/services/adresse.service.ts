a3dc505999d18f1f41b3dc3e5a5bc437532c3c24
Tram Anh <tramanh0105@gmail.com>
2019-06-03 20:26:03 +0200

    added adresseService

---
+++ src/app/services/adresse.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Adresse} from '../models/Adresse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdresseService {
+  url = `http://localhost:8081/users`;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAdresse(userId: number): Observable<Adresse> {
+    const newUrl = `${this.url}/${userId}/adresse`;
+    return this.http.get<Adresse>(newUrl);
+  }
+
+  createAdresse(userId: number, adresse: Adresse): Observable<Adresse> {
+    const newUrl = `${this.url}/${userId}/adresse`;
+    return this.http.post<Adresse>(newUrl, adresse);
+  }
+
+  deleteAdresse(userId: number): Observable<Adresse> {
+    const newUrl = `${this.url}/${userId}/adresse`;
+    return this.http.delete<Adresse>(newUrl);
+  }
+
+}
